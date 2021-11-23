56f3a224acd1deec9f5f8781567d0e3f9dc003d6
Diego Andrés Villa Cardona <diego.villa@ceiba.com.co>
2021-11-23 17:02:58 -0500

    fist models

---
+++ Domain/Entities/ImageData.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ImageData
+    {
+        [LoadColumn(0)]
+        public string ImagePath = default!;
+
+        [LoadColumn(1)]
+        public string Label = default!;
+    }
+}
